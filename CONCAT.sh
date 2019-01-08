9fd9939c5db0441ac09940bba2bc8e048a41d586
abhijeetsingh1704 <vishwaas1704@gmail.com>
2019-01-08 13:34:29 +0100

    Create CONCAT.sh

---
+++ CONCAT.sh
@@ -0,0 +1,17 @@
+#!/bin/bash/
+
+#This program makes concatanate sequence for the multifasta file
+#written by Abhijeet Singh {abhijeetsingh.aau@gmail.com}
+
+echo "Input your multifasta file name"
+read filename
+fasta_file=filename
+grep -v '^>' ${!fasta_file} > ${!fasta_file}_1
+perl -pe '/^>/ ? print "\n" : chomp' ${!fasta_file}_1 > ${!fasta_file}_12
+sed -e '1i\>Concatanate_sequence' ${!fasta_file}_12 > ${!fasta_file}_concatanate.fasta
+rm -f ${!fasta_file}_1 ${!fasta_file}_12
+
+echo "Your concatanated fasta sequence in the file names "
+echo "================================="
+echo "${!fasta_file}_concatanate.fasta"
+echo "================================="
